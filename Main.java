af1e6992a2a4da535f46fc578c1db25f875d484c
SYNAPSE\adam.gac <adam.gac@globallogic.com>
2019-04-09 12:17:21 +0200

    create simple jaxb html creator

---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.xml.bind.JAXBException;
+
+public class Main {
+
+    public static void main(String[] args) throws JAXBException {
+       TestEmployeeMarshing testEmployeeMarshing = new TestEmployeeMarshing();
+       testEmployeeMarshing.run();
+    }
+}
